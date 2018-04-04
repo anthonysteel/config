86fcaca2e11ce3747b03f677dc7ca0a6b74b2e00
anthonysteel <13as261@queensu.ca>
2018-04-04 15:27:33 -0500

    Print out status of package installation

--- set_up.sh
+++ set_up.sh
@@ -4,86 +4,39 @@ sudo add-apt-repository restricted
 sudo add-apt-repository multiverse
 
 sudo apt-get update
-if [ $? -eq 0 ]; then
-    echo "apt-get update"
-else
-    echo "apt-get failed to update"
-    #exit 1
-fi
 
 sudo apt-get install vim
-if [ $? -eq 0 ]; then
-    echo "vim installed"
-else
-    echo "vim failed to install"
-    #exit 1
-fi
+declare -A config=(["vim"]=$?)
 
 git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
-if [ $? -eq 0 ]; then
-    echo "vundle installed"
-else
-    echo "vundle failed to install"
-    #exit 1
-fi
+config+=(["vundle"]=$?)
 
 # Install vim plugins
 vim -c "PluginInstall" -c "qa"
 
 sudo apt-get install python-pip python-dev build-essential
-if [ $? -eq 0 ]; then
-    echo "pip installed"
-else
-    echo "pip failed to install"
-    #exit 1
-fi
+config+=(["python"]=$?)
 
 sudo pip install --upgrade pip
-if [ $? -eq 0 ]; then
-    echo "pip upgraded"
-else
-    echo "pip failed to upgrade"
-    #exit 1
-fi
+config+=(["pip"]=$?)
 
-pip install ipython
-if [ $? -eq 0 ]; then
-    echo "ipython installed"
-else
-    echo "ipython failed to install"
-    #exit 1
-fi
+sudo apt-get install ipython
+config+=(["ipython"]=$?)
 
 sudo pip install virtualenv virtualenvwrapper
-if [ $? -eq 0 ]; then
-    echo "virtualenvwrapper installed"
-else
-    echo "virtualenvwrapper failed to install"
-fi
+config+=(["virtualenv"]=$?)
 
-if [ $? -eq 0 ]; then
-    echo "nodejs installed"
-else
-    echo "nodejs failed to install"
-fi
+sudo apt-get install nodejs-legacy
+config+=(["virtualenv"]=$?)
 
 sudo apt-get install ruby-full
-if [ $? -eq 0 ]; then
-    echo "ruby installed"
-else
-    echo "ruby failed to install"
-    #exit 1
-fi
+config+=(["ruby-full"]=$?)
 
 sudo apt-get install tmux
+config+=(["tmux"]=$?)
 
-gem install tmuxinator
-if [ $? -eq 0 ]; then
-    echo "tmuxinator installed"
-else
-    echo "tmuxinator failed to install"
-    #exit 1
-fi
+sudo gem install tmuxinator
+config+=(["tmuxinator"]=$?)
 
 # terminal configuration
 for filename in ./config_files/*; do
@@ -94,12 +47,7 @@ done
 source ~/.bashrc
 
 sudo apt-get install chromium-browser
-if [ $? -eq 0 ]; then
-    echo "Chrome installed"
-else
-    echo "Chrome failed to install"
-    #exit 1
-fi
+config+=(["chromium"]=$?)
 
 sudo apt-get install dconf-cli
 git clone https://github.com/Anthony25/gnome-terminal-colors-solarized.git
@@ -107,3 +55,17 @@ cd gnome-terminal-colors-solarized
 ./install.sh
 
 sudo apt-get install fonts-powerline
+config+=(["powerline"]=$?)
+
+echo
+(
+printf '\e[1;38mPACKAGE\tSTATUS\n'
+for package in "${!config[@]}"; do
+    if [ "${config[$package]}" -eq 0 ]; then
+        printf '\e[1;33m%s\tInstalled\n' $package
+    else 
+        printf '\e[1;31m%s\tFailed\n' $package
+    fi
+done
+) | column -t -s $'\t'
+echo
